088d52e7bcc92bb5a69b205d459ea8e79bbc9401
vishal-panchal611 <vpanchal611@gmail.com>
2023-05-20 18:15:55 +0530

    Added css file

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 let firstCard = 11;
-let secondCard =10;
+let secondCard =1;
 
 let sum = firstCard + secondCard;
 
 let blackjak = false;
 let isAlive = true;
 
+let message =""
 console.log(sum)
 
 if(sum <= 20)
 {
-    console.log("Do you want to draw a card?")
+    message = "Do you want to draw a card?"
 }
 else if (sum === 21)
 {
     blackjak = true;
-    console.log("Wohoo ! You've got blackjack")
+    message = "Wohoo ! You've got blackjack"
 }
 else
 {
-    console.log("You're out of the game! ")
+    messgae = "You're out of the game!"
     isAlive = false;
 }
 
+
 console.log(blackjak)
+console.log(message)
