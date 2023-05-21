497d04013183ea18ab2ff8226ea3aa9c504bfa11
vishal-panchal611 <vpanchal611@gmail.com>
2023-05-21 18:15:32 +0530

    Added newCard function

--- index.js
+++ index.js
@@ -7,23 +7,42 @@ let blackjak = false;
 let isAlive = true;
 
 let message =""
-console.log(sum)
 
-if(sum <= 20)
-{
-    message = "Do you want to draw a card?"
-}
-else if (sum === 21)
-{
-    blackjak = true;
-    message = "Wohoo ! You've got blackjack"
-}
-else
-{
-    messgae = "You're out of the game!"
-    isAlive = false;
-}
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
+
+let sumEl = document.getElementById("sum-el")
+console.log(sumEl)
+
+let cardsEl = document.getElementById("cards-el")
+console.log(cardsEl)
 
 
-console.log(blackjak)
-console.log(message)
+function startGame()
+{
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    if(sum <= 20)
+    {
+        message = "Do you want to draw a card?"
+    }
+    else if (sum === 21)
+    {
+        blackjak = true;
+        message = "Wohoo ! You've got blackjack"
+    }
+    else
+    {
+        messgae = "You're out of the game!"
+        isAlive = false;
+    }
+    console.log(message)
+    messageEl.textContent = message
+
+}
+
+function newCard()
+{
+    console.log("Drawing a new card from the deck!")
+}
+//console.log(blackjak)
